50a532ad76011bdea752c3596fd40d26a4bace68
EviL <alvarosantosrom9@gmail.com>
2022-05-09 12:00:18 +0200

    :zap: Ultimos cambios antes de la entrega

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', "Zen's Dulce") }}</title>
+    <title>{{ config('app.name', "cafeteriaEsi") }}</title>
 
     
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', "Zen's Dulce") }}</title>
+    <title>{{ config('app.name', "cafeteriaEsi") }}</title>
 
     
     <meta name="csrf-token" content="{{ csrf_token() }}">
